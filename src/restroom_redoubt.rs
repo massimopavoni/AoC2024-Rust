24e6d70e08dd9bdf2306afa566758448faa398dc
Massimo Pavoni <maspavoni@gmail.com>
2024-12-16 15:14:17 +0100

    Better day 14

--- src/restroom_redoubt.rs
+++ src/restroom_redoubt.rs
@@ -1,12 +1,13 @@
 use itertools::Itertools;
+use num_modular::ModularUnaryOps;
 use regex::bytes::Regex;
 
-use crate::random_utils::re_match_atoi;
+use crate::random_utils::{pos::Pos, re_match_atoi};
 
 // ------------------------------------------------------------------------------------------------
 // Exports
 
-pub fn robots_safety_factor(input: &str) -> u64 {
+pub fn robots_safety_factor(input: &str) -> usize {
     let (mut qc1, mut qc2, mut qc3, mut qc4) = (0, 0, 0, 0);
 
     // Find robot positions after 100 seconds and calculate safety factor
@@ -14,16 +15,20 @@ pub fn robots_safety_factor(input: &str) -> u64 {
         .expect("Invalid regex")
         .captures_iter(input.as_bytes())
         .for_each(|captures| {
-            let (x, y, vx, vy) = (
-                re_match_atoi::<i64>(captures.get(1)),
-                re_match_atoi::<i64>(captures.get(2)),
-                re_match_atoi::<i64>(captures.get(3)),
-                re_match_atoi::<i64>(captures.get(4)),
+            let (position, velocity) = (
+                Pos::new(
+                    re_match_atoi::<isize>(captures.get(1)),
+                    re_match_atoi::<isize>(captures.get(2)),
+                ),
+                Pos::new(
+                    re_match_atoi::<isize>(captures.get(3)),
+                    re_match_atoi::<isize>(captures.get(4)),
+                ),
             );
 
             let (x, y) = (
-                (x + 100 * vx).rem_euclid(101),
-                (y + 100 * vy).rem_euclid(103),
+                (position.x + 100 * velocity.x).rem_euclid(101),
+                (position.y + 100 * velocity.y).rem_euclid(103),
             );
 
             match (x, y) {
@@ -38,37 +43,71 @@ pub fn robots_safety_factor(input: &str) -> u64 {
     qc1 * qc2 * qc3 * qc4
 }
 
-pub fn robots_christmas_tree(input: &str) -> i64 {
-    let robots_info = Regex::new(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)")
+#[allow(clippy::cast_precision_loss)]
+pub fn robots_christmas_tree(input: &str) -> usize {
+    let mut robots_info = Regex::new(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)")
         .expect("Invalid regex")
         .captures_iter(input.as_bytes())
         .map(|captures| {
             (
-                re_match_atoi::<i64>(captures.get(1)),
-                re_match_atoi::<i64>(captures.get(2)),
-                re_match_atoi::<i64>(captures.get(3)),
-                re_match_atoi::<i64>(captures.get(4)),
+                Pos::new(
+                    re_match_atoi::<isize>(captures.get(1)),
+                    re_match_atoi::<isize>(captures.get(2)),
+                ),
+                Pos::new(
+                    re_match_atoi::<isize>(captures.get(3)),
+                    re_match_atoi::<isize>(captures.get(4)),
+                ),
             )
         })
         .collect_vec();
 
-    // Find robot positions that minimize manhattan distance from the center
-    (101..101 * 103)
-        .map(|second| {
-            let mut total_manhattan_distance = 0;
+    // Find christmas tree by minimizing x and y variance and using the chinese remainder theorem
+    let (mut min_variance_x, mut min_variance_y) = (f64::MAX, f64::MAX);
+    let (mut min_second_x, mut min_second_y) = (0, 0);
 
-            for (x, y, vx, vy) in &robots_info {
+    for second in 1..104 {
+        let mut average = (0.0, 0.0);
+
+        robots_info = robots_info
+            .into_iter()
+            .map(|(position, velocity)| {
                 let (x, y) = (
-                    (x + second * vx).rem_euclid(101),
-                    (y + second * vy).rem_euclid(103),
+                    (position.x + velocity.x).rem_euclid(101),
+                    (position.y + velocity.y).rem_euclid(103),
                 );
 
-                total_manhattan_distance += (x - 50).abs() + (y - 51).abs();
-            }
+                average.0 += x as f64;
+                average.1 += y as f64;
+                (Pos::new(x, y), velocity)
+            })
+            .collect();
 
-            (total_manhattan_distance, second)
-        })
-        .min()
-        .expect("Expected christmas tree")
-        .1
+        average.0 /= robots_info.len() as f64;
+        average.1 /= robots_info.len() as f64;
+
+        let (mut variance_x, mut variance_y) = (0.0, 0.0);
+
+        for &(position, _) in &robots_info {
+            variance_x += (position.x as f64 - average.0).powi(2);
+            variance_y += (position.y as f64 - average.1).powi(2);
+        }
+
+        variance_x /= robots_info.len() as f64;
+        variance_y /= robots_info.len() as f64;
+
+        if variance_x < min_variance_x {
+            min_variance_x = variance_x;
+            min_second_x = second;
+        }
+
+        if variance_y < min_variance_y {
+            min_variance_y = variance_y;
+            min_second_y = second;
+        }
+    }
+
+    (min_second_x * 103 * 103.invm(&101_usize).expect("Expected modular inverse")
+        + min_second_y * 101 * 101.invm(&103_usize).expect("Expected modular inverse"))
+        % (101 * 103)
 }
